7ff77cbba3715a45230e6c5e48e769e1cce22bca
eriksgda <erik.ksgda@gmail.com>
2024-06-25 08:56:20 -0300

    add schema

--- todolist_api/app.py
+++ todolist_api/app.py
@@ -1,8 +1,12 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from todolist_api.schemas import Message
 
 app = FastAPI()
 
 
-@app.get("/")
-def readRoot() -> dict:
+@app.get("/", status_code=HTTPStatus.OK, response_model = Message)
+def readRoot():
     return {"message": "Hello World!"}
