41193503e17d3e9a9f4fc5edddc85315bb02ba3e
eriksgda <erik.ksgda@gmail.com>
2024-07-17 15:27:50 -0300

    create put and delete && tests

--- todolist_api/app.py
+++ todolist_api/app.py
@@ -1,8 +1,14 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
-from todolist_api.schemas import Message, UserDB, UserPublic, UserSchema
+from todolist_api.schemas import (
+    Message,
+    UserDB,
+    UserList,
+    UserPublic,
+    UserSchema,
+)
 
 app = FastAPI()
 
@@ -21,3 +27,34 @@ def create_user(user: UserSchema):
     database.append(user_with_id)
 
     return user_with_id
+
+
+@app.get("/users/", status_code=HTTPStatus.OK, response_model=UserList)
+def read_users():
+    return {"users": database}
+
+
+@app.put(
+    "users/{user_id}", status_code=HTTPStatus.OK, response_model=UserPublic
+)
+def update_user(user_id: int, user: UserSchema):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail="USER NOT FOUND"
+        )
+
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+    database[user_id - 1] = user_id
+
+    return user_with_id
+
+
+@app.delete("/users/{user_id}", response_model=Message)
+def delete_user(user_id: int):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail="USER NOT FOUND"
+        )
+
+    del database[user_id - 1]
+    return {"message": "User Deleted"}
