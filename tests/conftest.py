69bdead0cec94fabe2e85a80e638a00fb7730352
eriksgda <erik.ksgda@gmail.com>
2024-07-22 12:00:29 -0300

    update conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,19 +4,26 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
 from todolist_api.app import app
+from todolist_api.database import get_session
 from todolist_api.models import table_registry
 
 
 @pytest.fixture()
-def client():
-    return TestClient(app)
+def client(session):
+    def get_session_override():
+        return session
+
+    with TestClient(app) as client:
+        app.dependency_overrides[get_session] = get_session_override
+
+    yield client
+    app.dependency_overrides.clear()
 
 
 @pytest.fixture()
 def session():
     engine = create_engine("sqlite:///:memory:")
     table_registry.metadata.create_all(engine)
-
     with Session(engine) as session:
         yield session
 
