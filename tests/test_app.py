9cba8e2712b6403577b7f245f2693f4c4ae9770b
eriksgda <erik.ksgda@gmail.com>
2024-07-23 20:08:39 -0300

    create test for get token, and get token errors

--- tests/test_app.py
+++ tests/test_app.py
@@ -126,3 +126,42 @@ def test_delete_user_error(client):
 
     assert response.status_code == HTTPStatus.NOT_FOUND
     assert response.json() == {"detail": "User not found"}
+
+
+def test_get_token_error_with_password(client, user):
+    response = client.post(
+        "/token",
+        data={"username": user.email, "password": "senhaerrada"},
+    )
+
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {"detail": "Incorrect email or password"}
+
+
+def test_get_token_error_with_email(client, user):
+    response = client.post(
+        "/token",
+        data={
+            "username": "emailerrado@teste.com",
+            "password": "senhateste123",  # senha correta!
+        },
+    )
+
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {"detail": "Incorrect email or password"}
+
+
+def test_get_token(client, user):
+    response = client.post(
+        "/token",
+        data={
+            "username": user.email,
+            "password": "senhateste123",
+        },
+    )
+
+    return_token = response.json()
+
+    assert response.status_code == HTTPStatus.OK
+    assert return_token["token_type"] == "Bearer"
+    assert "access_token" in return_token
