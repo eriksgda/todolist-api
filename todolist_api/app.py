5fc03257900c99333ba8c49190ce09b28f0758ee
eriksgda <erik.ksgda@gmail.com>
2024-07-23 20:07:34 -0300

    create /token for login with token jwt

--- todolist_api/app.py
+++ todolist_api/app.py
@@ -1,6 +1,7 @@
 from http import HTTPStatus
 
 from fastapi import Depends, FastAPI, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy import func, or_, select
 from sqlalchemy.orm import Session
 
@@ -8,9 +9,15 @@ from todolist_api.database import get_session
 from todolist_api.models import User
 from todolist_api.schemas import (
     Message,
+    Token,
     UserList,
     UserPublic,
     UserSchema,
+)
+from todolist_api.security import (
+    create_access_token,
+    get_password_hash,
+    verify_password,
 )
 
 app = FastAPI()
@@ -42,7 +49,9 @@ def create_user(user: UserSchema, session: Session = Depends(get_session)):
             )
 
     db_user = User(
-        username=user.username, email=user.email, password=user.password
+        username=user.username,
+        email=user.email,
+        password=get_password_hash(user.password),
     )
     db_user.update_at = func.now()
 
@@ -87,7 +96,7 @@ def update_user(
         )
 
     db_user.username = user.username
-    db_user.password = user.password
+    db_user.password = get_password_hash(user.password)
     db_user.email = user.email
     session.commit()
     session.refresh(db_user)
@@ -108,3 +117,21 @@ def delete_user(user_id: int, session: Session = Depends(get_session)):
     session.commit()
 
     return {"message": "User deleted"}
+
+
+@app.post("/token/", response_model=Token)
+def login_for_acess_token(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    session: Session = Depends(get_session),
+):
+    user = session.scalar(select(User).where(User.email == form_data.username))
+
+    if not user or not verify_password(form_data.password, user.password):
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail="Incorrect email or password",
+        )
+
+    access_token = create_access_token(payload_data={"sub": user.email})
+
+    return {"access_token": access_token, "token_type": "Bearer"}
