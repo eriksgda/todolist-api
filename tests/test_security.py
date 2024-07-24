97f38c674249c55f5853301c6d0c5b09edd06d71
eriksgda <erik.ksgda@gmail.com>
2024-07-23 21:59:57 -0300

    create test for invalid token and invalid user (error in select)

--- tests/test_security.py
+++ tests/test_security.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 from jwt import decode
 
 from todolist_api.security import ALGORITHM, SECRET_KEY, create_access_token
@@ -11,3 +13,22 @@ def test_create_jwt_token():
 
     assert result["sub"] == data["sub"]
     assert result["exp"]
+
+
+def test_jwt_invalid_token(client):
+    response = client.delete(
+        "/users/1", headers={"Authorization": "Bearer token-invalido"}
+    )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {"detail": "Could not validate credentials"}
+
+
+def test_jwt_invalid_user(client, user, token):
+    user.email = ""
+    response = client.delete(
+        f"users/{user.id}", headers={"Authorization": f"Bearer {token}"}
+    )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {"detail": "Could not validate credentials"}
