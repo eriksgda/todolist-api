14fddb9df557c7d8adb5e3a56b3345b5fa1869de
eriksgda <erik.ksgda@gmail.com>
2024-07-24 19:31:53 -0300

    file refactored

--- todolist_api/app.py
+++ todolist_api/app.py
@@ -1,143 +1,15 @@
 from http import HTTPStatus
 
-from fastapi import Depends, FastAPI, HTTPException
-from fastapi.security import OAuth2PasswordRequestForm
-from sqlalchemy import func, or_, select
-from sqlalchemy.orm import Session
+from fastapi import FastAPI
 
-from todolist_api.database import get_session
-from todolist_api.models import User
-from todolist_api.schemas import (
-    Message,
-    Token,
-    UserList,
-    UserPublic,
-    UserSchema,
-)
-from todolist_api.security import (
-    create_access_token,
-    get_current_user,
-    get_password_hash,
-    verify_password,
-)
+from todolist_api.routers import auth, users
+from todolist_api.schemas import Message
 
 app = FastAPI()
+app.include_router(users.router)
+app.include_router(auth.router)
 
 
 @app.get("/", status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {"message": "Hello World!"}
-
-
-@app.post("/users/", status_code=HTTPStatus.CREATED, response_model=UserPublic)
-def create_user(user: UserSchema, session: Session = Depends(get_session)):
-    db_user = session.scalar(
-        select(User).where(
-            or_(User.username == user.username, User.email == user.email)
-        )
-    )
-
-    if db_user:
-        if db_user.username == user.username:
-            raise HTTPException(
-                status_code=HTTPStatus.BAD_REQUEST,
-                detail="Username already exists",
-            )
-        elif db_user.email == user.email:
-            raise HTTPException(
-                status_code=HTTPStatus.BAD_REQUEST,
-                detail="Email already exists",
-            )
-
-    db_user = User(
-        username=user.username,
-        email=user.email,
-        password=get_password_hash(user.password),
-    )
-    db_user.update_at = func.now()
-
-    session.add(db_user)
-    session.commit()
-    session.refresh(db_user)
-
-    return db_user
-
-
-@app.get("/users/", status_code=HTTPStatus.OK, response_model=UserList)
-def read_users(
-    session: Session = Depends(get_session), limit: int = 10, offset: int = 0
-):
-    users = session.scalars(select(User).limit(limit).offset(offset))
-    return {"users": users}
-
-
-@app.get(
-    "/users/{user_id}", status_code=HTTPStatus.OK, response_model=UserPublic
-)
-def read_user_by_id(user_id: int, session: Session = Depends(get_session)):
-    user = session.scalar(select(User).where(User.id == user_id))
-    if not user:
-        raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail="User not found"
-        )
-
-    return user
-
-
-@app.put(
-    "/users/{user_id}", status_code=HTTPStatus.OK, response_model=UserPublic
-)
-def update_user(
-    user_id: int,
-    user: UserSchema,
-    session: Session = Depends(get_session),
-    current_user=Depends(get_current_user),
-):
-    if current_user.id != user_id:
-        raise HTTPException(
-            status_code=HTTPStatus.BAD_REQUEST, detail="Not enough permission"
-        )
-
-    current_user.username = user.username
-    current_user.password = get_password_hash(user.password)
-    current_user.email = user.email
-
-    session.commit()
-    session.refresh(current_user)
-
-    return current_user
-
-
-@app.delete("/users/{user_id}", response_model=Message)
-def delete_user(
-    user_id: int,
-    session: Session = Depends(get_session),
-    current_user=Depends(get_current_user),
-):
-    if current_user.id != user_id:
-        raise HTTPException(
-            status_code=HTTPStatus.BAD_REQUEST, detail="Not enough permission"
-        )
-
-    session.delete(current_user)
-    session.commit()
-
-    return {"message": "User deleted"}
-
-
-@app.post("/token/", response_model=Token)
-def login_for_acess_token(
-    form_data: OAuth2PasswordRequestForm = Depends(),
-    session: Session = Depends(get_session),
-):
-    user = session.scalar(select(User).where(User.email == form_data.username))
-
-    if not user or not verify_password(form_data.password, user.password):
-        raise HTTPException(
-            status_code=HTTPStatus.BAD_REQUEST,
-            detail="Incorrect email or password",
-        )
-
-    access_token = create_access_token(payload_data={"sub": user.email})
-
-    return {"access_token": access_token, "token_type": "Bearer"}
