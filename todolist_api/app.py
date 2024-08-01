a1d3fa263c863ec19f1a5e686bd9b3e1f4f67346
eriksgda <erik.ksgda@gmail.com>
2024-08-01 20:33:27 -0300

    link new router in app

--- todolist_api/app.py
+++ todolist_api/app.py
@@ -2,12 +2,13 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
-from todolist_api.routers import auth, users
+from todolist_api.routers import auth, todos, users
 from todolist_api.schemas import Message
 
 app = FastAPI()
 app.include_router(users.router)
 app.include_router(auth.router)
+app.include_router(todos.router)
 
 
 @app.get("/", status_code=HTTPStatus.OK, response_model=Message)
