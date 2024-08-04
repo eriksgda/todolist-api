a4666351074371f3e7c803dfafdc42119221939d
eriksgda <erik.ksgda@gmail.com>
2024-08-04 16:10:57 -0300

    create read_todo endpoint with query

--- todolist_api/routers/todos.py
+++ todolist_api/routers/todos.py
@@ -1,11 +1,13 @@
+from http import HTTPStatus
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query, HTTPException
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from todolist_api.database import get_session
 from todolist_api.models import Todo, User
-from todolist_api.schemas import TodoPublic, TodoSchema
+from todolist_api.schemas import TodoList, TodoPublic, TodoSchema, TodoUpdate
 from todolist_api.security import get_current_user
 
 router = APIRouter(prefix="/todos", tags=["todos"])
@@ -30,3 +32,30 @@ def create_todo(
     session.refresh(db_todo)
 
     return db_todo
+
+
+@router.get("/", response_model=TodoList)
+def read_todos(  # noqa
+    session: T_Session,
+    current_user: T_Current_User,
+    title: str = Query(None),
+    description: str = Query(None),
+    state: str = Query(None),
+    offset: int = Query(None),
+    limit: int = Query(None),
+):
+    query = select(Todo).where(Todo.user_id == current_user.id)
+
+    if title:
+        query = query.filter(Todo.title.contains(title))
+
+    if description:
+        query = query.filter(Todo.description.contains(description))
+
+    if state:
+        query = query.filter(Todo.state == state)
+
+    todos = session.scalars(query.offset(offset).limit(limit)).all()
+
+    return {"todos": todos}
+
