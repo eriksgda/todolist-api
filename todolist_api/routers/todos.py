b1679df8706923a2dac62c3d8e0ad98aa2998951
eriksgda <erik.ksgda@gmail.com>
2024-08-04 15:13:32 -0300

    create new router (toDo)

--- todolist_api/routers/todos.py
+++ todolist_api/routers/todos.py
@@ -1,4 +1,32 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from todolist_api.database import get_session
+from todolist_api.models import Todo, User
+from todolist_api.schemas import TodoPublic, TodoSchema
+from todolist_api.security import get_current_user
 
 router = APIRouter(prefix="/todos", tags=["todos"])
 
+T_Session = Annotated[Session, Depends(get_session)]
+T_Current_User = Annotated[User, Depends(get_current_user)]
+
+
+@router.post("/", response_model=TodoPublic)
+def create_todo(
+    todo: TodoSchema, current_user: T_Current_User, session: T_Session
+):
+    db_todo = Todo(
+        title=todo.title,
+        description=todo.description,
+        state=todo.state,
+        user_id=current_user.id,
+    )
+
+    session.add(db_todo)
+    session.commit()
+    session.refresh(db_todo)
+
+    return db_todo
