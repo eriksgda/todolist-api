c5fd1cf442481170231518b4cb7644bca276f16c
eriksgda <erik.ksgda@gmail.com>
2024-08-04 15:15:40 -0300

    create test to ToDo in user db

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import select
 
-from todolist_api.models import User
+from todolist_api.models import Todo, User
 
 
 def test_create_user(session):
@@ -16,3 +16,20 @@ def test_create_user(session):
     )
 
     assert result.username == "teste"
+
+
+def test_create_todo(session, user: User):
+    todo = Todo(
+        title="Test Todo",
+        description="Test Desc",
+        state="draft",
+        user_id=user.id,
+    )
+
+    session.add(todo)
+    session.commit()
+    session.refresh(todo)
+
+    user = session.scalar(select(User).where(User.id == user.id))
+
+    assert todo in user.todos
